e696f846d97e2f70d9ec9fc6ac11013957487d29
jamesjbustos <45052719+jamesjbustos@users.noreply.github.com>
2023-04-22 13:11:12 -0400

    Update 4_📈_CSV_Q&A.py

--- pages/4_📈_CSV_Q&A.py
+++ pages/4_📈_CSV_Q&A.py
@@ -1,12 +1,12 @@
 # Import libraries
+import streamlit as st
 import pandas as pd
 import os
-
-# Llama Index & Langchain libraries
-from llama_index import download_loader, GPTSimpleVectorIndex, LLMPredictor, PromptHelper, ServiceContext
-from langchain import OpenAI
-from llama_index import download_loader
+import pinecone
 from pathlib import Path
+from llama_index import (download_loader, LLMPredictor,
+                         PromptHelper, ServiceContext, GPTPineconeIndex)
+from langchain import OpenAI
 
 # Streamlit page configurations and title
 st.set_page_config(
@@ -16,39 +16,46 @@ st.set_page_config(
 st.title("📈 CSV Q&A")
 st.caption("🌟 Your personal CSV data assistant - upload and start asking questions!")
 
-# Path to documents and index file
-doc_path = './CSVdata/'
-index_file = 'CSVindex.json'
-
 index = None
 
 # Load API Key
-
 api_key = st.secrets["OPENAI_API_KEY"]
-
+pinecone_api_key = st.secrets["PINECONE_API_KEY"]
+pinecone_enviroment = st.secrets["PINECONE_ENVIRONMENT"]
 
 # ------ Initialize Session State ------
 if 'csv_response' not in st.session_state:
     st.session_state.csv_response = ''
-    
-    
+
+# ------ Helper functions ------
+def save_uploaded_file(uploadedfile):
+    with open(os.path.join("data", uploadedfile.name), "wb") as f:
+        f.write(uploadedfile.getbuffer())
+
 # ------ Load and index document ------
-uploaded_file = st.file_uploader(" ", accept_multiple_files=False, label_visibility='collapsed')
+uploaded_file = st.file_uploader(" ", accept_multiple_files=False,
+                                  label_visibility='collapsed', type=['csv'])
 
 if uploaded_file is not None:
-    # Clear existing documents
-    for doc_file in os.listdir(doc_path):
-        os.remove(os.path.join(doc_path, doc_file))
-    
-    # Save uploaded file to doc_path
-    with open(os.path.join(doc_path, uploaded_file.name), 'wb') as f:
-        f.write(uploaded_file.read())
+    # Create data folder if it doesn't exist
+    if not os.path.exists('./data'):
+        os.mkdir('./data')
+
+    # Save uploaded file to data folder
+    save_uploaded_file(uploaded_file)
 
     # Load and index document
-    uploaded_file_path = os.path.join(doc_path, uploaded_file.name)
+    uploaded_file_path = os.path.join('data', uploaded_file.name)
     SimpleCSVReader = download_loader("SimpleCSVReader")
     loader = SimpleCSVReader()
     documents = loader.load_data(file=Path(uploaded_file_path))
+    
+    # Pinecone intialization
+    pinecone.init(
+        api_key=pinecone_api_key, 
+        environment=pinecone_enviroment
+    )
+    pinecone_index = pinecone.Index("langchainjsfundamentals")
     
     # Display uploaded CSV file as DataFrame (new lines added)
     df = pd.read_csv(uploaded_file_path)
@@ -59,18 +66,13 @@ if uploaded_file is not None:
     prompt_helper = PromptHelper(max_input_size=4096, num_output=256, max_chunk_overlap=20)
     service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
     
-    index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
-    
-    # Save index to file
-    index.save_to_disk(index_file)
-
-elif os.path.exists(index_file):
-    index = GPTSimpleVectorIndex.load_from_disk(index_file)
-    
-    SimpleCSVReader = download_loader("SimpleCSVReader")
-    loader = SimpleCSVReader()
-    uploaded_file_path = os.path.join(doc_path, os.listdir(doc_path)[0])
-    documents = loader.load_data(file=Path(uploaded_file_path))
+    # Create the GPTPineconeIndex
+    index = GPTPineconeIndex.from_documents(
+        documents,
+        pinecone_index=pinecone_index,
+        service_context=service_context,
+        add_sparse_vector=True,
+    )
 
 if index is not None:
     with st.form("chat_form", clear_on_submit=True):
