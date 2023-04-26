75908d04bde96d10021898a17686c9eb55cb0f24
jamesjbustos <45052719+jamesjbustos@users.noreply.github.com>
2023-04-25 23:21:57 -0400

    Update 5_📈_CSV_Q&A.py

--- pages/5_📈_CSV_Q&A.py
+++ pages/5_📈_CSV_Q&A.py
@@ -2,11 +2,8 @@
 import streamlit as st
 import pandas as pd
 import os
-import pinecone
-from pathlib import Path
-from llama_index import (download_loader, LLMPredictor,
-                         PromptHelper, ServiceContext, GPTPineconeIndex)
 from langchain import OpenAI
+from langchain.agents import create_csv_agent
 
 # Streamlit page configurations and title
 st.set_page_config(
@@ -16,12 +13,8 @@ st.set_page_config(
 st.title("📈 CSV Q&A")
 st.caption("✨ Your personal CSV data assistant - upload and start asking questions!")
 
-index = None
-
 # Load API Key
 api_key = st.secrets["OPENAI_API_KEY"]
-pinecone_api_key = st.secrets["PINECONE_API_KEY"]
-pinecone_enviroment = st.secrets["PINECONE_ENVIRONMENT"]
 
 # ------ Initialize Session State ------
 if 'csv_response' not in st.session_state:
@@ -46,35 +39,14 @@ if uploaded_file is not None:
 
     # Load and index document
     uploaded_file_path = os.path.join('data', uploaded_file.name)
-    SimpleCSVReader = download_loader("SimpleCSVReader")
-    loader = SimpleCSVReader()
-    documents = loader.load_data(file=Path(uploaded_file_path))
-
-    # Pinecone intialization
-    pinecone.init(
-        api_key=pinecone_api_key,
-        environment=pinecone_enviroment
-    )
-    pinecone_index = pinecone.Index("studygpt")
+    
+    # Create agent
+    agent = create_csv_agent(OpenAI(temperature=0), uploaded_file_path, verbose=True)
 
     # Display uploaded CSV file as DataFrame
     df = pd.read_csv(uploaded_file_path)
     st.dataframe(df)
 
-    # Define llm and index
-    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
-    prompt_helper = PromptHelper(max_input_size=4096, num_output=256, max_chunk_overlap=20)
-    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-
-    # Create the GPTPineconeIndex
-    index = GPTPineconeIndex.from_documents(
-        documents,
-        pinecone_index=pinecone_index,
-        service_context=service_context,
-        add_sparse_vector=True,
-    )
-
-if index is not None:
     with st.form("chat_form", clear_on_submit=True):
         col1, col2 = st.columns([10, 1])
         user_prompt = col1.text_area(" ", max_chars=2000, key="prompt",
@@ -83,6 +55,7 @@ if index is not None:
 
     if submitted and user_prompt:
         with st.spinner("💭 Waiting for response..."):
-            st.session_state.csv_response = index.query(user_prompt)
+            st.session_state.csv_response = agent.run(user_prompt)
         response_md = f"🤖 **AI:** {st.session_state.csv_response}\n\n"
         st.markdown(response_md)
+        
